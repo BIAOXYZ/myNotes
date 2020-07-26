0b05d76c21c4abcdc39fef10bde4faf790cc6553
BIAOXYZ <BIAOXYZLL@GMAIL.COM>
2020-07-27 02:47:01 +0800

    Create functools.py

---
+++ programming_notes/programming_languages/prog_lang_python/Python_packages/functools.py
@@ -0,0 +1,8 @@
+
+# 官方
+
+functools --- 高阶函数和可调用对象上的操作 https://docs.python.org/zh-cn/3/library/functools.html || functools — Higher-order functions and operations on callable objects https://docs.python.org/3/library/functools.html
+
+# 其他
+
+Python Functools – lru_cache() https://www.geeksforgeeks.org/python-functools-lru_cache/
